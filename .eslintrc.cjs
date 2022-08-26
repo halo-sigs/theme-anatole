c41ea5dd3e9acfec77ee87611e46c698db742c1c
Ryan Wang <i@ryanc.cc>
2022-08-26 10:53:18 +0800

    feat: configure eslint and prettier for astro components
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,6 +8,17 @@ module.exports = {
     "eslint:recommended",
     "@vue/eslint-config-typescript/recommended",
     "@vue/eslint-config-prettier",
+    "plugin:astro/recommended",
+  ],
+  overrides: [
+    {
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+    },
   ],
   env: {
     node: true,
--- prettier.config.js
+++ prettier.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  plugins: [require("prettier-plugin-tailwindcss")],
+  plugins: [
+    require("prettier-plugin-tailwindcss"),
+    require("prettier-plugin-astro"),
+  ],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
 };
